9473f7ba8397c0e472c46840e22ec5436f18360c
Luke Nzang'u <saintlukke@gmail.com>
2017-06-20 14:23:52 +0300

    Finish setting up user interface using flask

--- bucklist/views.py
+++ bucklist/views.py
@@ -11,3 +11,24 @@ def index():
     Return and render index.html template.
     """
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    Return and render register.html template.
+    """
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    """
+    Return and render login.html template.
+    """
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    """
+    Return and render home.html template.
+    """
+    return render_template('home.html')
