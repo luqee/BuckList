0e33e8c5a4fadc5e14618f5082c368e2b1ac00b6
Luke Nzang'u <saintlukke@gmail.com>
2017-06-23 08:20:22 +0300

    Create forms for adding bucketlist and items

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import DataRequired, Email
 
 
@@ -20,3 +20,21 @@ class LoginForm(Form):
 	password = PasswordField('Password',
 	 validators=[DataRequired('Please enter a password')])
 	submit = SubmitField('Sign in')
+
+class CreateBucketForm(Form):
+	name = StringField('Name',
+	 	validators=[DataRequired('Please provide your Bucketlist\'s name')])
+	description = StringField('Description',
+		validators=[DataRequired('Please provide your description')])
+	date = StringField('Date',
+		validators=[DataRequired('Please provide the date')])
+	submit = SubmitField('Create')
+
+class CreateItemForm(Form):
+	title = StringField('Title',
+	 	validators=[DataRequired('Please provide your Bucketlist Item name')])
+	description = StringField('Description',
+		validators=[DataRequired('Please provide your description')])
+	date = StringField('Date',
+		validators=[DataRequired('Please provide the date')])
+	submit = SubmitField('Create Item')
