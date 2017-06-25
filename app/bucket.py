e62efd8a9bd4dd2da1db212ac0f605a4b1144a87
Luke Nzang'u <saintlukke@gmail.com>
2017-06-25 16:01:15 +0300

    Fix bugs

--- app/bucket.py
+++ app/bucket.py
@@ -1,4 +1,10 @@
 class Application(object):
+    ''' This class models a bucket list application
+
+    It provides functionalities for users to register and log in
+    One can also create and delete bucketlist and items
+    '''
+
     # Dictionary mapping an email address to a User object
     _email_to_user_map = {}
     # A list of signed in user's email address
@@ -11,63 +17,96 @@ class Application(object):
         return cls.instance
 
     def register_user(self, user):
+        '''
+        This method takes the user object to register to the application
+        and adds the object to _email_to_user_map dictionary.
+        It returns the string "Email exists" if user is already registered
+        or the the string "Registered" if successfully registered.
+        '''
         if user.email in self._email_to_user_map.keys():
-            return False
+            return 'Email exists'
         else:
             self._email_to_user_map[user.email] = user
-            return True
+            return 'Registered'
 
     def login_user(self, email, password):
-        if password == '':
-            return False
-        elif email == '':
-            return False
+        '''
+        This method takes the email address and the password as credentials
+        and checks from the  _email_to_user_map if the user exists.
+        If so the user's email addreess
+        is added to the list of logged in users and
+        the function returns True. It returns False otherwise.
+        '''
         if email in self._email_to_user_map.keys():
-            if password == self._email_to_user_map[email].password:
+            if password == self._email_to_user_map[email].password and password not in self._signed_in_users:
                 self._signed_in_users.append(email)
                 return True
             else:
                 return False
-
-    '''
-    This method takes email and bucket list object
-    as parameters and inserts both in the _email_to_bucket_list_map
-    provided the email exists in the _email_to_user_map and.
-    '''
-    def create_bucket_list(self, email, bucklist):
+    
+    def logout_user(self, email):
+        '''
+        This method takes the user's email address as an argument
+        and cheks if they are registered and are signed in and if so
+        the user is removed from the signed in users list.
+        '''
         if email in self._email_to_user_map.keys():
-            user = self._email_to_user_map[email]
-            user.buck_lists.append(bucklist)
-            return True
+            if email in self._signed_in_users:
+                self._signed_in_users.remove(email)
+                return True
         else:
             return False
 
-    '''
-    This method takes email and returns the associated list
-    of a user's bucketlist objects
-    '''
+    def create_bucket_list(self, email, bucklist):
+        '''
+        This method takes email and bucket list object
+        as parameters and inserts both in the _email_to_bucket_list_map
+        provided the email exists in the _email_to_user_map and.
+        '''
+        if email in self._email_to_user_map.keys():
+            user = self._email_to_user_map[email]
+            bucklist.id = len(user.buck_lists) + 1
+            user.buck_lists.append(bucklist)
+            return True
+        return False
+
+
     def get_bucket_lists(self, email):
+        '''
+        This method takes email and returns the associated list
+        of a user's bucketlist objects
+        '''
         if email in self._email_to_user_map.keys():
             user = self._email_to_user_map[email]
             bucklists = user.buck_lists
             return bucklists
+        return False
+
+
+    def remove_bucket_list(self, email, buck_id):
+        '''
+        This method takes as arguments the variables email and bucket list id
+        and returns True if the bucket list is deleted or
+        False otherwise.
+        '''
+        if email in self._email_to_user_map.keys():
+            for buck in self._email_to_user_map[email].buck_lists:
+                if buck.id == buck_id:
+                    self._email_to_user_map[email].buck_lists.remove(buck)
+                    return True
         else:
-            return 0
-
-
-    def remove_bucket_list(email, buck_id):
-            if email in self._email_to_user_map.keys():
-                for buck in self._email_to_user_map[email].buck_lists:
-                    if buck.id == buck_id:
-                        self._email_to_user_map[email].buck_lists.remove(buck)
-                        return True
-            else:
-                return False
+            return False
 
 
     def view_bucket_list_items(self, email, buck_id):
+        '''
+        This method gets the bicketlist items
+
+        params: email
+        params: bucket list id
+        '''
         if email in self._email_to_user_map.keys():
-            for bucket_list in self._email_to_user_map[email].buck_lists
+            for bucket_list in self._email_to_user_map[email].buck_lists:
                 if bucket_list.id == buck_id:
                     return bucket_list.items
         else:
@@ -75,6 +114,14 @@ class Application(object):
 
 
     def create_bucket_list_item(self, email, item, buck_id):
+        ''' Creates a bucketlist item
+
+        params: email of the logged in user
+        params: item to add to bucket list
+        params: bucket list id on which to add the item
+
+        return: boolean
+        '''
         if email in self._email_to_user_map.keys():
             for buck in self._email_to_user_map[email].buck_lists:
                 if buck.id == buck_id:
@@ -85,9 +132,17 @@ class Application(object):
             return False
 
 
-    def remove_bucket_list_item(email, buck_id, item_id):
+    def remove_bucket_list_item(self, email, buck_id, item_id):
+        ''' Removes an item from the bucket list
+
+        params: email
+        params: bucket list id
+        params: item id
+
+        return: boolean
+        '''
         if email in self._email_to_user_map.keys():
-            for buck in _email_to_user_map[email].buck_lists:
+            for buck in self._email_to_user_map[email].buck_lists:
                 if buck.id == buck_id:
                     for item in buck.items:
                         if item.id == item_id:
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, flash, session, redirect
+from flask import render_template, request, flash, session, redirect, url_for
 from app import app, models, forms
 from app import bucket
+import pdb
 """
 This module defines the routes to be used by the flask application instance.
 """
@@ -44,9 +45,16 @@ def register():
             user = models.User(request.form['first_name'],
                 request.form['last_name'],request.form['email'],
                 request.form['password'])
-            app = bucket.Application()
-            if app.register_user(user):
+            reg = bucket.Application().register_user(user)
+            if reg == 'Registered' :
+                flash('You have been successfully registered.')
                 return redirect(url_for('login'))
+            elif reg == 'Email exists':
+                flash('User with the given Email already exists!.')
+                return render_template('register.html', form=form)
+            else:
+                flash('Something went very wrong.')
+                render_template('register.html', form=form)
     elif request.method == 'GET':
         return render_template('register.html', form=form)
 
@@ -54,7 +62,10 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
-    Return and render login.html template.
+    Return and render login.html template if the method is GET.
+    If the method is POST, the form form is validated and a Log is attempted.
+    On successfull login the user is redirected to the home page.
+    Else the user is redirected back to the home page.
     """
     form = forms.LoginForm()
     if request.method == 'POST':
@@ -67,8 +78,9 @@ def login():
             app = bucket.Application()
             if app.login_user(email, password):
                 session['email'] = email
+                pdb.set_trace()
                 flash('You were successfully logged in')
-                return redirect('home.html')
+                return redirect(url_for('home'))
             else:
                 flash('Invalid User name or password')
                 return render_template('login.html', form=form)
@@ -78,39 +90,51 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    app = bucket.Application()
-    app.logout_user(session['email'])
-    session.pop(sesion['email'], None)
-    return redirect(url_for('index'))
+    '''
+    This logs the user out of the application
+    '''
+    if session.get('email', None) is not None:
+        buck_app = bucket.Application()
+        pdb.set_trace()
+        res = buck_app.logout_user(session['email'])
+        if res is True:
+            pdb.set_trace()
+            email = session.pop(session['email'], None)
+            flash(email + ' has beensuccessfully logged out')
+            return redirect(url_for('index'))
+        else:
+            pdb.set_trace()
+            return redirect('login')
 
-
+# p bucket.Application()._signed_in_users
 @app.route('/home')
 def home():
-    """
-    Return and render home.html template.
-    """
-    if session('email', None) == None:
+    """Return and render home.html template."""
+    if session.get('email', None) is not None:
+        buck_app = bucket.Application()
+        bucket_lists = buck_app.get_bucket_lists(session['email'])
+        if bucket_lists != False:
+            return render_template('home.html', bucket_lists=bucket_lists)
+    else:
         flash('You need to log in first')
         return redirect(url_for('login'))
-    else:
-        app = bucket.Application()
-        bucket_lists = app.get_bucket_lists(session['email'])
-        return render_template('home.html', bucket_lists=bucket_lists)
 
 
 @app.route('/createBuck', methods=['GET', 'POST'])
 def create_bucket():
     """
-    Return and render team.html template.
+    Return and render create_bucket.html template if GET
+    If POST create the bucket list.
     """
     form = forms.CreateBucketForm()
     if request.method == 'POST':
-        bucket_list = models.BucketList(request.form['name'], request.form['description'], request.form['date'],)
-        app = bucket.Application()
-        res = app.create_bucket_list(session['email'], bucket_list)
+        bucket_list = models.BucketList(request.form['name'],
+        request.form['description'], request.form['date'],)
+        buck_app = bucket.Application()
+        res = buck_app.create_bucket_list(session['email'], bucket_list)
         if res == True:
             flash('BucketList created successfully')
-            return render_template('home.html')
+            return redirect('home')
         else:
             flash('Something went wrong!!')
             return render_template('create_bucklist.html', form=form)
@@ -121,11 +145,9 @@ def create_bucket():
 
 @app.route('/removeBuck/<int:buck_id>')
 def remove_bucket(buck_id):
-    """
-    Return and render team.html template.
-    """
-    app = bucket.Application()
-    res = app.remove_bucket_list(sesion['email'], buck_id)
+    """ Removes a the user's bucketlist."""
+    buck_app = bucket.Application()
+    res = buck_app.remove_bucket_list(session['email'], buck_id)
     if res:
         flash('successfully deleted')
         return render_template('home.html')
@@ -135,11 +157,9 @@ def remove_bucket(buck_id):
 
 @app.route('/viewBuck/<int:buck_id>')
 def view_bucket(buck_id):
-    """
-    Return and render team.html template.
-    """
-    app = bucket.Application()
-    res = app.view_bucket_list_items(sesion['email'], buck_id)
+    """ Returns the page to view bucketlist items."""
+    buck_app = bucket.Application()
+    res = buck_app.view_bucket_list_items(session['email'], buck_id)
     if isinstance(res, list):
         return render_template('items.html', items=res, buck_id=buck_id)
     else:
@@ -148,30 +168,30 @@ def view_bucket(buck_id):
 
 
 @app.route('/createItem/<int:buck_id>', methods=['GET', 'POST'])
-def create_item():
+def create_item(buck_id):
     """
-    Return and render team.html template.
+    Return and render create_item.html template if GET
+    otherwise process item creation.
     """
     form = forms.CreateItemForm()
     if request.method == 'POST':
-        item = models.BucketListItem(request.form['title'], request.form['description'], request.form['date'],)
-        app = bucket.Application()
-        res = app.create_bucket_list_item(session['email'], item, buck_id)
+        item = models.BucketListItem(request.form['title'],
+        request.form['description'], request.form['date'],)
+        buck_app = bucket.Application()
+        res = buck_app.create_bucket_list_item(session['email'], item, buck_id)
         if res == True:
             flash('Item  created successfully in BucketList')
-            return render_template('home.html')
+            return redirect(url_for('view_bucket', buck_id=buck_id))
         else:
             flash('Something went wrong!!')
-            return render_template('create_item.html')
+            return render_template('create_item.html', form=form)
     elif request.method == 'GET':
-        return render_template('create_item.html')
+        return render_template('create_item.html', form=form)
 
 
 @app.route('/removeItem/<int:buck_id>/<int:item_id>')
-def remove_item():
-    """
-    Return and render team.html template.
-    """
+def remove_item(buck_list, item_id):
+    """ remove Item and redirect to  """
     app = bucket.Application()
     res = app.remove_bucket_list_item(sesion['email'], buck_id, item_id)
     if res:
